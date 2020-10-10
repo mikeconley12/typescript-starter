56172b12196a3acbc6a6221394c7e377aa18d13e
mikeconley12 <46870881+mikeconley12@users.noreply.github.com>
2020-10-10 20:59:14 +0300

    Create child.provider.ts

---
+++ src/child.provider.ts
@@ -0,0 +1,10 @@
+import { ParentProvider } from './parent.provider';
+import { OtherProvider } from './other-provider';
+
+// @Injectable()
+export class ChildProvider extends ParentProvider {
+  constructor(otherProvider: OtherProvider) {
+    console.log("otherProvider", otherProvider);
+    super(null);
+  }
+}
